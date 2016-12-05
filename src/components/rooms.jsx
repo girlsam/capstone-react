dd6b183e31b46395e87186d06f6daf7fa59d2805
Sam Goldsmith <samggold@gmail.com>
2016-12-05 13:14:43 -0700

    Carousel and Users for Rooms

--- src/components/rooms.jsx
+++ src/components/rooms.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import Fetch from 'isomorphic-fetch';
-import { Col, Card, CardTitle } from 'react-materialize';
+import { Grid, Row, Pager, Jumbotron, Button, Carousel, CarouselItem, CarouselCaption, Col } from 'react-bootstrap';
+import _ from 'lodash';
 
 //components
-import NavigationBar from './NavBar';
+import NavbarInstance from './NavBar';
 
 const URL='https://newmate.herokuapp.com';
 
@@ -24,24 +25,68 @@ export default class Room extends Component {
       return res.json()
     })
     .then(json => {
-      this.setState({rooms: json})
+      this.setState({rooms: json.results.rooms})
     });
   }
 
   componentDidMount(rooms) {
     this.getData(rooms);
-    console.log('this', this);
-    console.log('state', this.state);
-    console.log('rooms', this.state.rooms);
+  }
+
+  getInitialState() {
+    return {
+      index: 0,
+      direction: null
+    };
+  }
+
+  handleSelect(selectedIndex, e) {
+    this.setState({
+      index: selectedIndex,
+      direction: e.direction
+    });
   }
 
   render() {
 
     return (
       <div>
-        <NavigationBar />
+        <NavbarInstance />
+          <Pager>
+            <Pager.Item href="#">Post from Mates</Pager.Item>
+              {' '}
+            <Pager.Item href="#">Posts from the Web</Pager.Item>
+          </Pager>
         <div className="container-fluid">
-            Test
+          {this.state.rooms.map(el => {
+            return (
+              <Col xs={6} md={6}>
+                <Jumbotron key={el.id}>
+                  <Carousel activeIndex={this.state.index} direction={this.state.direction} onSelect={this.handleSelect}>
+                    <Carousel.Item>
+                      <img width={900} height={500} alt="900x500" src={el.image_1}/>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                      <img width={900} height={500} alt="900x500" src={el.image_2}/>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                      <img width={900} height={500} alt="900x500" src={el.image_3}/>
+                    </Carousel.Item>
+                  </Carousel>
+                  <h4 className="card-name">{el.bedrooms} Bed, {el.bathrooms} Bath</h4>
+                  <p className="card-descr">{el.description}</p>
+                  <Grid>
+                  <Row>
+                    <Col md={6} mdOffset={6}>
+                      <img className="rooms-image" src={el.image}/><p className="rooms-user-descr">Posted by: {el.first_name}</p>
+                    </Col>
+                    <p></p>
+                  </Row>
+                  </Grid>
+                </Jumbotron>
+              </Col>
+              )
+            })}
         </div>
       </div>
     )
